a8187bd9d799dfa078e4bed2e490d007f841c463
Ortars1 <ortars@yandex.ru>
2025-05-30 03:06:35 +0300

    Final version

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: 'react',
-      bundler: 'webpack'
-    }
+  e2e: {
+    baseUrl: "http://localhost:4000",
+    specPattern: "cypress/e2e/**/*.cy.{ts,tsx}",
+    supportFile: "cypress/support/e2e.ts",
+    viewportWidth: 1280,
+    viewportHeight: 720
   }
 });
